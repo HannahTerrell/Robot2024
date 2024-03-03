2fe9fe57e4d7deedd35ba52799a7b23f574a05d4
Hannah <hterrell24@student.gccsmi.org>
2024-03-03 14:23:12 -0500

    new auto (untested)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -97,27 +97,29 @@ public class RobotContainer {
     NamedCommands.registerCommand("feedAndShoot", feedAndShoot);
 
     //Auton things
-    final PathPlannerAuto m_pathplanner1 = new PathPlannerAuto("One-Amp Auto");
-    final PathPlannerAuto m_pathplanner2 = new PathPlannerAuto("Two-Amp Auto");
+    // final PathPlannerAuto m_pathplanner1 = new PathPlannerAuto("One-Amp Auto");
+    // final PathPlannerAuto m_pathplanner2 = new PathPlannerAuto("Two-Amp Auto");
     final PathPlannerAuto m_pathplanner3 = new PathPlannerAuto("Two-Speaker Auto");
-    final PathPlannerAuto m_pathplanner4 = new PathPlannerAuto("Demo Auto");
-    final PathPlannerAuto m_pathplanner5 = new PathPlannerAuto("Speaker-Podium Auto (Non-Amp)");
-    final PathPlannerAuto m_pathplanner6 = new PathPlannerAuto("Speaker-Podium Auto (Center)");
-    final PathPlannerAuto m_pathplanner7 = new PathPlannerAuto("Disruption Auto");
-    final PathPlannerAuto m_pathplanner8 = new PathPlannerAuto("Test Auto");
+    // final PathPlannerAuto m_pathplanner4 = new PathPlannerAuto("Demo Auto");
+    // final PathPlannerAuto m_pathplanner5 = new PathPlannerAuto("Speaker-Podium Auto (Non-Amp)");
+    // final PathPlannerAuto m_pathplanner6 = new PathPlannerAuto("Speaker-Podium Auto (Center)");
+    // final PathPlannerAuto m_pathplanner7 = new PathPlannerAuto("Disruption Auto");
+    // final PathPlannerAuto m_pathplanner8 = new PathPlannerAuto("Test Auto");
+    final PathPlannerAuto m_pathplanner9 = new PathPlannerAuto("Out Auto");
 
     //Auton chooser
     m_autonChooser = new SendableChooser<>();
 
     //Adding auton routines to chooser
-    m_autonChooser.addOption("One-Amp Auton", m_pathplanner1);
-    m_autonChooser.addOption("Two-Amp Auton", m_pathplanner2);
+    // m_autonChooser.addOption("One-Amp Auton", m_pathplanner1);
+    // m_autonChooser.addOption("Two-Amp Auton", m_pathplanner2);
     m_autonChooser.addOption("Two-Speaker Auton", m_pathplanner3);
-    m_autonChooser.addOption("Demo Auton", m_pathplanner4);
-    m_autonChooser.addOption("Speaker-Podium Auton (Non-Amp)", m_pathplanner5);
-    m_autonChooser.addOption("Speaker-Podium Auton (Center)", m_pathplanner6);
-    m_autonChooser.addOption("Disruption Auton", m_pathplanner7);
-    m_autonChooser.addOption("Test Auton", m_pathplanner8);
+    // m_autonChooser.addOption("Demo Auton", m_pathplanner4);
+    // m_autonChooser.addOption("Speaker-Podium Auton (Non-Amp)", m_pathplanner5);
+    // m_autonChooser.addOption("Speaker-Podium Auton (Center)", m_pathplanner6);
+    // m_autonChooser.addOption("Disruption Auton", m_pathplanner7);
+    // m_autonChooser.addOption("Test Auton", m_pathplanner8);
+    m_autonChooser.addOption("Out Auton", m_pathplanner9);
     SmartDashboard.putData("Auton Chooser", m_autonChooser);
   }
 
