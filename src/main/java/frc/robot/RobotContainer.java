47d4ca7cdba9a0c4360e774222f0fa15e4fa66a9
HSRobotics2 <hsrobotics2@gccsmi.org>
2024-03-09 17:24:06 -0500

    two-speaker auto works! trying three-speaker

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -30,7 +30,6 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  private XCaliper m_robot;
 
   //Subsystems
   private final Drivetrain m_swerve = new Drivetrain();
@@ -43,22 +42,20 @@ public class RobotContainer {
   //Commands
   private final ShootSpeaker shootSpeaker = new ShootSpeaker(m_shooter);
   private final AimArm aimArmContinuous = new AimArm(m_arm, m_limelight, true);
+  private final AimArm aimArm = new AimArm(m_arm, m_limelight, false);
   private final ArmDown armDown = new ArmDown(m_arm);
   private final FeedAndShoot feedAndShoot = new FeedAndShoot(m_shooter, m_intake);
   private final IntakeAndFeed intakeAndFeed = new IntakeAndFeed(m_shooter, m_intake);
   private final StopSystems stopSystems = new StopSystems(m_shooter, m_intake, m_arm);
-  //private final AmpScore ampScore = new AmpScore(m_shooter, armUp, armDown);
 
   //Auton chooser initiation
   SendableChooser<Command> m_autonChooser;
 
   //Controllers
-  //private final Joystick m_driverController = new Joystick(OperatorConstants.kDriverControllerPort);
   private final XboxController m_driverController = new XboxController(0);
   private final XboxController m_operatorController = new XboxController(1);
 
   //Buttons and axes
-  // private final int m_intakeAxis = XboxController.Axis.kLeftY.value;
   private final int m_armAxis = XboxController.Axis.kRightY.value;
   private final JoystickButton m_climbUpButton = new JoystickButton(m_operatorController, XboxController.Button.kLeftBumper.value);
   private final JoystickButton m_climbDownButton = new JoystickButton(m_operatorController, XboxController.Button.kRightBumper.value);
@@ -75,18 +72,16 @@ public class RobotContainer {
 
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer(XCaliper robot) {
+  public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
-
-    m_robot = robot;
 
     //Commands for PathPlanner
     NamedCommands.registerCommand("stopModules", new InstantCommand(() -> {m_swerve.stopModules();}));
     NamedCommands.registerCommand("stopSystems", stopSystems);
-    NamedCommands.registerCommand("shootSpeaker", new RunCommand(() -> {m_shooter.shootSpeaker();}).withTimeout(1.0));
+    NamedCommands.registerCommand("shootSpeaker", shootSpeaker);
     NamedCommands.registerCommand("shootAmp", new RunCommand(() -> {m_shooter.shootAmp();}).withTimeout(1.0));
-    // NamedCommands.registerCommand("armUp", armUp);
+    NamedCommands.registerCommand("aimArm", aimArm);
     NamedCommands.registerCommand("armDown", armDown);
     NamedCommands.registerCommand("intakeAndFeed", intakeAndFeed);
     NamedCommands.registerCommand("feedAndShoot", feedAndShoot);
@@ -95,7 +90,7 @@ public class RobotContainer {
     final PathPlannerAuto m_pathplanner1 = new PathPlannerAuto("One-Amp Auto");
     // final PathPlannerAuto m_pathplanner2 = new PathPlannerAuto("Two-Amp Auto");
     final PathPlannerAuto m_pathplanner3 = new PathPlannerAuto("Two-Speaker Auto");
-    // final PathPlannerAuto m_pathplanner4 = new PathPlannerAuto("Demo Auto");
+    final PathPlannerAuto m_pathplanner4 = new PathPlannerAuto("Three-Speaker Auto");
     // final PathPlannerAuto m_pathplanner5 = new PathPlannerAuto("Speaker-Podium Auto (Non-Amp)");
     // final PathPlannerAuto m_pathplanner6 = new PathPlannerAuto("Speaker-Podium Auto (Center)");
     // final PathPlannerAuto m_pathplanner7 = new PathPlannerAuto("Disruption Auto");
@@ -109,7 +104,7 @@ public class RobotContainer {
     m_autonChooser.addOption("One-Amp Auton", m_pathplanner1);
     // m_autonChooser.addOption("Two-Amp Auton", m_pathplanner2);
     m_autonChooser.addOption("Two-Speaker Auton", m_pathplanner3);
-    // m_autonChooser.addOption("Demo Auton", m_pathplanner4);
+     m_autonChooser.addOption("Three-Speaker Auton", m_pathplanner4);
     // m_autonChooser.addOption("Speaker-Podium Auton (Non-Amp)", m_pathplanner5);
     // m_autonChooser.addOption("Speaker-Podium Auton (Center)", m_pathplanner6);
     // m_autonChooser.addOption("Disruption Auton", m_pathplanner7);
@@ -123,7 +118,6 @@ public class RobotContainer {
       new RunCommand(() -> {
         var intakeSpeed = (m_operatorController.getRawAxis(XboxController.Axis.kRightTrigger.value)
            - m_operatorController.getRawAxis(XboxController.Axis.kLeftTrigger.value));
-        // var intakeSpeed = -(m_operatorController.getRawAxis(m_intakeAxis));
         m_intake.intake(intakeSpeed);
       },
       m_intake));
@@ -220,21 +214,11 @@ public class RobotContainer {
   }
 
   private void driveWithJoystick(boolean fieldRelative) {
-    // Get the x speed. We are inverting this because Xbox controllers return
-    // negative values when we push forward.
+
     double xSpeed = MathUtil.applyDeadband(-m_driverController.getLeftY(), 0.02);
 
-    // Get the y speed or sideways/strafe speed. We are inverting this because
-    // we want a positive value when we pull to the left. Xbox controllers
-    // return positive values when you pull to the right by default.
     double ySpeed = MathUtil.applyDeadband(-m_driverController.getLeftX(), 0.02);
 
-    // Get the rate of angular rotation. We are inverting this because we want a
-    // positive value when we pull to the left (remember, CCW is positive in
-    // mathematics). Xbox controllers return positive values when you pull to
-    // the right by default.
-    // var rotWODeadband = m_driverController.getRightTriggerAxis() 
-    //   - m_driverController.getLeftTriggerAxis();
     double rotWODeadband = -m_driverController.getRightX();
     double rot = MathUtil.applyDeadband(rotWODeadband, 0.02);
 
--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -17,7 +17,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Arm extends SubsystemBase {
     private CANSparkMax m_armMotor = new CANSparkMax(16, MotorType.kBrushless);
     private RelativeEncoder m_armEncoder = m_armMotor.getEncoder();
-    private PIDController m_positionController = new PIDController(0.07, 0, 0);
+    private PIDController m_positionController = new PIDController(0.19, 0, 0);
     private boolean m_stopped;
 
     // be careful setting this to low (slow) because it also prevents slowing down.
@@ -41,7 +41,6 @@ public class Arm extends SubsystemBase {
     }
 
     public void setAimpointAmp() {
-        // TODO: what posotion should this be?
         m_positionController.setSetpoint(MAX_SETPOINT);
         m_stopped = false;
     }
@@ -67,7 +66,7 @@ public class Arm extends SubsystemBase {
 
         // this is a linear formula, and doesn't account for a ballistic arc.
         // it would be good to have a couple distances with encoder measurements here.
-        var setpoint = (distance / 2) * 13.3;
+        var setpoint = (distance / 4) * 13.3;
         setpoint = MathUtil.clamp(setpoint, 0, MAX_SETPOINT);
 
         m_positionController.setSetpoint(setpoint);
@@ -75,8 +74,7 @@ public class Arm extends SubsystemBase {
     }
 
     public boolean getIsAtSetpoint() {
-        // TODO: does this work right?
-        return Math.abs(m_positionController.getPositionError()) < .01;
+        return Math.abs(m_positionController.getPositionError()) < 0.03;
     }
 
     public void stop() {
